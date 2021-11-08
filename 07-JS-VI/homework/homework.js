e5b5f130503014004e32ee354d44532b607cde94
Franco <francofyzz15@gmail.com>
2021-11-08 14:09:38 -0300

    commit 07

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -26,30 +26,45 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
-  suma = 0;
-  for(var i = 0; i < numeros.length; i++) {
-    suma = suma + numeros[i];
-  }
-  cb(suma);
+      // suma = 0;
+      // for(var i = 0; i < numeros.length; i++) {
+      //  suma = suma + numeros[i];
+      //}
+      //cb(suma);
+  var result = numeros.reduce(function(acumulador, element) {
+    return acumulador + element
+  });
+  cb(result)
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(function(element) {
+    cb(element);
+  });
 }
 
 function map(array, cb) {
   // Crea un nuevo array
-  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
+  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` 
+  // en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var nuevoArray = array.map(cb)
+  return nuevoArray
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  result = array.filter(funcion)
+  function funcion(value) {
+    return value[0] === 'a'
+  }
+  return result;
 }
 
 // No modificar nada debajo de esta línea
